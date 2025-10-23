658b8f83d0b01aeae9638ea100fc3d7f0625fc63
Shadekade <shadekade@mail.ru>
2025-10-23 11:44:57 +0500

    Add files via upload

---
+++ Lab_rab_4Kirichenko/View/WindowRole.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using Lab_rab_4Kirichenko.ViewModel;
+
+namespace Lab_rab_4Kirichenko.View
+{
+    /// <summary>
+    /// Логика взаимодействия для WindowRole.xaml
+    /// </summary>
+    public partial class WindowRole : Window
+    {
+        public WindowRole()
+        {
+            InitializeComponent();
+            RoleViewModel vmRole = new RoleViewModel();
+            lvRole.ItemsSource = vmRole.ListRole;
+        }
+    }
+}
---
+++ Lab_rab_4Kirichenko/ViewModel/RoleViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Lab_rab_4Kirichenko.Model;
+
+namespace Lab_rab_4Kirichenko.ViewModel
+{
+    public class RoleViewModel
+    {
+        public ObservableCollection<Role> ListRole { get; set; } = new ObservableCollection<Role>();
+
+        public RoleViewModel()
+        {
+            this.ListRole.Add(new Role { Id = 1, NameRole = "Директор" });
+            this.ListRole.Add(new Role { Id = 2, NameRole = "Бухгалтер" });
+            this.ListRole.Add(new Role { Id = 3, NameRole = "Менеджер" });
+        }
+    }
+}
