64461641a1cd8d57125ffeeb4f68e36c4cf73b59
Shadekade <shadekade@mail.ru>
2026-04-08 20:28:00 +0500

    Add files via upload

---
+++ Lab_rab_3_Kirichenko/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Lab_rab_3_Kirichenko.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ Lab_rab_3_Kirichenko/Models/Vegetable.cs
@@ -0,0 +1,8 @@
+﻿namespace Lab_rab_3_Kirichenko.Models
+{
+    public class Vegetable
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
